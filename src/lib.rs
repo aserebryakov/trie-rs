a8be09c55e5c600990445a5c714d91aa17206758
Alexander Serebryakov <alex.serebr@gmail.com>
2017-12-06 22:35:40 +0200

    Adds Trie structure and interface to create a new empty trie

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,46 @@
+pub mod trie {
+    use std::rc::Rc;
+    use std::cell::RefCell;
+
+
+    pub struct Trie {
+        root : TrieNode,
+    }
+
+
+    struct TrieNode {
+        key : char,
+        val : u64,
+        children : Vec<Rc<RefCell<TrieNode>>>,
+    }
+
+
+    impl Trie {
+        pub fn new() -> Trie {
+            Trie {
+                root : TrieNode {
+                    key : '\0',
+                    val : 0u64,
+                    children : Vec::<Rc<RefCell<TrieNode>>>::new(),
+                }
+            }
+        }
+
+
+        pub fn empty(&self) -> bool {
+            self.root.children.len() == 0
+        }
+    }
+}
+
+
+
 #[cfg(test)]
 mod tests {
+    use trie;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn new_trie_is_empty() {
+        assert_eq!(trie::Trie::new().empty(), true);
     }
 }
