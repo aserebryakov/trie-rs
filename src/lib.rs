a331c5f3a1662555c39432998cfc5c7523b5ecaf
Alexander Serebryakov <alex.serebr@gmail.com>
2017-12-06 23:02:17 +0200

    Adds stubs for API and tests

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,19 @@ pub mod trie {
         pub fn empty(&self) -> bool {
             self.root.children.len() == 0
         }
+
+
+        pub fn add(&mut self, key: &[char]) {
+        }
+
+
+        pub fn clear(&mut self) {
+        }
+
+
+        pub fn has_key(&self, key: &[char]) -> bool {
+            false
+        }
     }
 }
 
@@ -42,5 +55,15 @@ mod tests {
     #[test]
     fn new_trie_is_empty() {
         assert_eq!(trie::Trie::new().empty(), true);
+    }
+
+    #[test]
+    fn add_word_to_trie() {
+        let mut t = trie::Trie::new();
+        let data : Vec<char> = "test".chars().collect();
+        t.add(&data[..]);
+
+        assert_eq!(t.empty(), false);
+        assert_eq!(t.has_key(&data[..]), true);
     }
 }
