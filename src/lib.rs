1911bc858391b2ad160faf09af2f2a8d8afd4601
Alexander Serebryakov <alex.serebr@gmail.com>
2017-12-07 10:12:03 +0200

    Completes the basic API and tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,27 +3,38 @@ pub mod trie {
     use std::cell::RefCell;
 
 
-    pub struct Trie {
-        root : TrieNode,
+    struct TrieNode {
+        key: char,
+        children: Vec<Rc<RefCell<TrieNode>>>,
     }
 
 
-    struct TrieNode {
-        key : char,
-        val : u64,
-        children : Vec<Rc<RefCell<TrieNode>>>,
+    impl TrieNode {
+        pub fn new(key: char) -> TrieNode {
+            TrieNode {
+                key,
+                children: Vec::<Rc<RefCell<TrieNode>>>::new(),
+            }
+        }
+
+
+        pub fn find(key: char) -> Option<Rc<RefCell<TrieNode>>> {
+            None
+        }
+
+
+        pub fn add(key: char) {}
+    }
+
+
+    pub struct Trie {
+        root: TrieNode,
     }
 
 
     impl Trie {
         pub fn new() -> Trie {
-            Trie {
-                root : TrieNode {
-                    key : '\0',
-                    val : 0u64,
-                    children : Vec::<Rc<RefCell<TrieNode>>>::new(),
-                }
-            }
+            Trie { root: TrieNode::new('\0') }
         }
 
 
@@ -33,11 +44,11 @@ pub mod trie {
 
 
         pub fn add(&mut self, key: &[char]) {
+            for c in key {}
         }
 
 
-        pub fn clear(&mut self) {
-        }
+        pub fn clear(&mut self) {}
 
 
         pub fn has_key(&self, key: &[char]) -> bool {
@@ -57,13 +68,44 @@ mod tests {
         assert_eq!(trie::Trie::new().empty(), true);
     }
 
+
     #[test]
     fn add_word_to_trie() {
         let mut t = trie::Trie::new();
-        let data : Vec<char> = "test".chars().collect();
+        let data: Vec<char> = "test".chars().collect();
+        t.add(&data[..]);
+
+        assert_eq!(t.empty(), false);
+    }
+
+
+    #[test]
+    fn has_key_test() {
+        let mut t = trie::Trie::new();
+        let data: Vec<char> = "test".chars().collect();
+        let another_data: Vec<char> = "nottest".chars().collect();
+
         t.add(&data[..]);
 
         assert_eq!(t.empty(), false);
         assert_eq!(t.has_key(&data[..]), true);
+        assert_eq!(t.has_key(&another_data[..]), false);
+    }
+
+
+    #[test]
+    fn clear_test() {
+        let mut t = trie::Trie::new();
+        let data: Vec<char> = "test".chars().collect();
+
+        t.add(&data[..]);
+
+        assert_eq!(t.empty(), false);
+        assert_eq!(t.has_key(&data[..]), true);
+
+        t.clear();
+
+        assert_eq!(t.empty(), true);
+        assert_eq!(t.has_key(&data[..]), false);
     }
 }
