04ecaf62687308702edb8f922ea3d0e71551657c
Alexander Serebryakov <alex.serebr@gmail.com>
2017-12-07 17:54:16 +0200

    Adds the basic documentation to the library

--- src/trie.rs
+++ src/trie.rs
@@ -46,22 +46,55 @@ impl TrieNode {
 }
 
 
+/// Prefix tree object
 pub struct Trie {
     root: Rc<RefCell<TrieNode>>,
 }
 
 
 impl Trie {
+    /// Creates a new `Trie` object
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use trie::trie::Trie;
+    ///
+    /// let t = Trie::new();
+    /// ```
     pub fn new() -> Trie {
         Trie { root: Rc::new(RefCell::new(TrieNode::new('\0'))) }
     }
 
 
-    pub fn empty(&self) -> bool {
+    /// Checks that trie is empty
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use trie::trie::Trie;
+    ///
+    /// let t = Trie::new();
+    /// assert_eq!(t.is_empty(), true);
+    /// ```
+    pub fn is_empty(&self) -> bool {
         self.root.borrow().children.is_empty()
     }
 
 
+    /// Adds a new key to the trie
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use trie::trie::Trie;
+    ///
+    /// let mut t = Trie::new();
+    /// let data: Vec<char> = "test".chars().collect();
+    ///
+    /// t.add(&data[..]);
+    /// assert_eq!(t.is_empty(), false);
+    /// ```
     pub fn add(&mut self, key: &[char]) {
         let mut node = self.root.clone();
         for c in key {
@@ -71,11 +104,42 @@ impl Trie {
     }
 
 
+    /// Clears the trie
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use trie::trie::Trie;
+    ///
+    /// let mut t = Trie::new();
+    /// let data: Vec<char> = "test".chars().collect();
+    ///
+    /// t.add(&data[..]);
+    /// t.clear();
+    /// assert_eq!(t.is_empty(), true);
+    /// ```
     pub fn clear(&mut self) {
         (*self.root).borrow_mut().children.clear();
     }
 
 
+    /// Looks for the key in trie
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use trie::trie::Trie;
+    ///
+    /// let mut t = Trie::new();
+    /// let data: Vec<char> = "test".chars().collect();
+    /// let another_data: Vec<char> = "notintest".chars().collect();
+    ///
+    /// t.add(&data[..]);
+    ///
+    /// assert_eq!(t.is_empty(), false);
+    /// assert_eq!(t.has_key(&data[..]), true);
+    /// assert_eq!(t.has_key(&another_data[..]), false);
+    /// ```
     pub fn has_key(&self, key: &[char]) -> bool {
         let mut node = self.root.clone();
         for c in key {
@@ -83,7 +147,7 @@ impl Trie {
 
             match node.borrow().find(c) {
                 Some(child) => _next_node = child,
-                    None => return false,
+                None => return false,
             }
 
             node = _next_node;
--- tests/api_tests.rs
+++ tests/api_tests.rs
@@ -6,8 +6,8 @@ mod tests {
     use trie::trie::Trie;
 
     #[test]
-    fn new_trie_is_empty() {
-        assert_eq!(Trie::new().empty(), true);
+    fn new_trie_is_is_empty() {
+        assert_eq!(Trie::new().is_empty(), true);
     }
 
 
@@ -17,7 +17,7 @@ mod tests {
         let data: Vec<char> = "test".chars().collect();
         t.add(&data[..]);
 
-        assert_eq!(t.empty(), false);
+        assert_eq!(t.is_empty(), false);
     }
 
 
@@ -25,11 +25,11 @@ mod tests {
     fn has_key_test() {
         let mut t = Trie::new();
         let data: Vec<char> = "test".chars().collect();
-        let another_data: Vec<char> = "nottest".chars().collect();
+        let another_data: Vec<char> = "notintest".chars().collect();
 
         t.add(&data[..]);
 
-        assert_eq!(t.empty(), false);
+        assert_eq!(t.is_empty(), false);
         assert_eq!(t.has_key(&data[..]), true);
         assert_eq!(t.has_key(&another_data[..]), false);
     }
@@ -42,12 +42,12 @@ mod tests {
 
         t.add(&data[..]);
 
-        assert_eq!(t.empty(), false);
+        assert_eq!(t.is_empty(), false);
         assert_eq!(t.has_key(&data[..]), true);
 
         t.clear();
 
-        assert_eq!(t.empty(), true);
+        assert_eq!(t.is_empty(), true);
         assert_eq!(t.has_key(&data[..]), false);
     }
 }
