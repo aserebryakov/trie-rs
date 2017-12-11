ec8a930794da292a9ca96509fa63bfb40c71722d
Alexander Serebryakov <alex.serebr@gmail.com>
2017-12-11 18:24:39 +0200

    Adds the documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,35 @@
 //!
 //! Trie is the library that implements the [trie](https://en.wikipedia.org/wiki/Trie).
 //!
-//! Trie is a generic data structure, written `Trie<T>`.
+//! Trie is a generic data structure, written `Trie<T, U>` where `T` is node key type and `U` is a
+//! value type.
+//!
+//! # Motivation
+//!
+//! Trie may be faster than other data structures in some cases e.g. if trie is used as a
+//! dictionary, than checking the presence of the word in dictionary may be significantly faster
+//! than hash map if the most of searches are not for words in the dictionary.
+//!
+//! # Usage
+//!
+//! ```rust
+//! use trie::Trie;
+//!
+//! let mut t = Trie::new();
+//!
+//! t.add("this".chars(), 1);
+//! t.add("trie".chars(), 2);
+//! t.add("contains".chars(), 3);
+//! t.add("a".chars(), 4);
+//! t.add("number".chars(), 5);
+//! t.add("of".chars(), 6);
+//! t.add("words".chars(), 7);
+//!
+//! assert_eq!(t.has_key("number".chars()), true);
+//! assert_eq!(t.has_key("not_existing_key".chars()), false);
+//! assert_eq!(t.get_value("words".chars()), Some(7));
+//! assert_eq!(t.get_value("none".chars()), None);
+//! ```
 
 mod trie_node;
 use std::rc::Rc;
